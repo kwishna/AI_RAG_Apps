5df5a6af189dde20af955338f3a66822b8426e0a
kwishna <kwishna@gmail.com>
2024-04-03 03:25:06 +0530

    1st commit

---
+++ RAG--001/.ipynb_checkpoints/ollama-python-sample-checkpoint.py
@@ -0,0 +1,35 @@
+#pip install ollama
+
+import ollama
+response = ollama.chat(model='gemma:2b-instruct', messages=[
+  {
+    'role': 'user',
+    'content': 'Why is the sky blue?',
+  },
+])
+print(response['message']['content'])
+
+#--------------------------------------------------
+
+resp = ollama.generate(model='llama2', prompt='Why is the sky blue?')
+print(resp)
+
+#--------------------------------------------------
+
+emb = ollama.embeddings(model='gemma:2b-instruct', prompt='They sky is blue because of rayleigh scattering')
+print(emb)
+
+#--------------------------------------------------
+
+import ollama
+
+stream = ollama.chat(
+    model='gemma:2b-instruct',
+    messages=[{'role': 'user', 'content': 'Why is the sky blue?'}],
+    stream=True,
+)
+
+for chunk in stream:
+  print(chunk['message']['content'], end='', flush=True)
+
+#--------------------------------------------------
