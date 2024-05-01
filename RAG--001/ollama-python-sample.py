97d39f39f565fd02b2089bd5f8030fecc7261303
kwishna <kwishna@gmail.com>
2024-05-01 20:28:50 +0530

    self-rag & corrective rag

--- RAG--001/ollama-python-sample.py
+++ RAG--001/ollama-python-sample.py
@@ -1,33 +1,34 @@
-#pip install ollama
+# pip install ollama
 
 import ollama
-response = ollama.chat(model='gemma:2b-instruct', messages=[
-  {
-    'role': 'user',
-    'content': 'Why is the sky blue?',
-  },
+
+response = ollama.chat(model='phi3:latest', messages=[
+    {
+        'role': 'user',
+        'content': 'Why is the sky blue?',
+    },
 ])
 print(response['message']['content'])
 
-#--------------------------------------------------
+# --------------------------------------------------
 
-resp = ollama.generate(model='llama2', prompt='Why is the sky blue?')
+resp = ollama.generate(model='phi3:latest', prompt='Why is the sky blue?')
 print(resp)
 
-#--------------------------------------------------
+# --------------------------------------------------
 
-emb = ollama.embeddings(model='gemma:2b-instruct', prompt='They sky is blue because of rayleigh scattering')
+emb = ollama.embeddings(model='phi3:latest', prompt='They sky is blue because of rayleigh scattering')
 print(emb)
 
-#--------------------------------------------------
+# --------------------------------------------------
 
 stream = ollama.chat(
-    model='gemma:2b-instruct',
+    model='phi3:latest',
     messages=[{'role': 'user', 'content': 'Why is the sky blue?'}],
     stream=True,
 )
 
 for chunk in stream:
-  print(chunk['message']['content'], end='', flush=True)
+    print(chunk['message']['content'], end='', flush=True)
 
-#--------------------------------------------------
+# --------------------------------------------------
