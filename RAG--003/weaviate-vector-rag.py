80cfdd19762176a375e3b275d423eb534113e25a
kwishna <kwishna@gmail.com>
2024-05-03 00:22:56 +0530

    weaviate vector db - using cloud service

--- RAG--003/weaviate-vector-rag.py
+++ RAG--003/weaviate-vector-rag.py
@@ -1,14 +1,13 @@
+import os
 import dotenv
-import weaviate
 import requests
-from weaviate.embedded import EmbeddedOptions
-from langchain.text_splitter import CharacterTextSplitter
-from langchain_community.embeddings import OpenAIEmbeddings
-from langchain_community.vectorstores import Weaviate
+import weaviate
 from langchain.prompts import ChatPromptTemplate
-from langchain_community.chat_models import ChatOpenAI
-from langchain.schema.runnable import RunnablePassthrough
 from langchain.schema.output_parser import StrOutputParser
+from langchain.schema.runnable import RunnablePassthrough
+from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter
+from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAIEmbeddings
 from langchain_community.document_loaders import TextLoader
 from langchain_weaviate.vectorstores import WeaviateVectorStore
 
@@ -22,58 +21,44 @@ with open("state_of_the_union.txt", "w") as f:
 loader = TextLoader('./state_of_the_union.txt')
 documents = loader.load()
 
+# ----------------
+
+# weaviate_client = weaviate.connect_to_local()
+weaviate_client = weaviate.connect_to_wcs(cluster_url="https://my-vector-392mwjps.weaviate.network", auth_credentials=weaviate.classes.init.Auth.api_key(os.environ['WEAVIATE_API_KEY']),)
+vectorstore = WeaviateVectorStore.from_documents(documents,  OpenAIEmbeddings(), client=weaviate_client)
 
 # -------------------
 with open("state_of_the_union.txt") as f:
     state_of_the_union = f.read()
 
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+text_splitter = RecursiveCharacterTextSplitter(separators=['\n', ' ', '.'], chunk_size=500, chunk_overlap=20)
 texts = text_splitter.split_text(state_of_the_union)
 
-weaviate_client = weaviate.connect_to_local()
+# ----------------------
 
-docsearch = WeaviateVectorStore.from_texts(
+docsearch_vector_store = WeaviateVectorStore.from_texts(
     texts,
     OpenAIEmbeddings(),
     client=weaviate_client,
     metadatas=[{"source": f"{i}-pl"} for i in range(len(texts))],
 )
 
-# -----------
-
-# text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=50)
-# chunks = text_splitter.split_documents(documents)
-#
-# client = weaviate.Client(
-#   embedded_options = EmbeddedOptions()
-# )
-#
-# vectorstore = Weaviate.from_documents(
-#     client = client,
-#     documents = chunks,
-#     embedding = OpenAIEmbeddings(),
-#     by_text = False
-# )
-#
-# retriever = vectorstore.as_retriever()
-
-vectorstore = WeaviateVectorStore.from_documents(documents,  OpenAIEmbeddings(), client=weaviate_client)
-
 # ------ similarity search ------
 query = "What did the president say about Justice Breyer"
-docs = vectorstore.similarity_search(query, k=10)
-# Print the first 100 characters of each result
+docs = docsearch_vector_store.similarity_search(query, k=10)
+
+# Print the first 100 characters of each of the 10 results.
 for i, doc in enumerate(docs):
     print(f"\nDocument {i+1}:")
     print(doc.page_content[:100] + "...")
 
 # ------ score ------
-docs = vectorstore.similarity_search_with_score("country", k=5)
+docs = docsearch_vector_store.similarity_search_with_score("country", k=5)
 for doc in docs:
     print(f"{doc[1]:.3f}", ":", doc[0].page_content[:100] + "...")
 
-#------ retriever ------
-retriever = vectorstore.as_retriever(search_type="mmr")
+#------- retriever -------
+retriever = docsearch_vector_store.as_retriever(search_type="mmr")
 
 template = """You are an assistant for question-answering tasks. 
 Use the following pieces of retrieved context to answer the question. 
@@ -96,5 +81,5 @@ rag_chain = (
     | StrOutputParser()
 )
 
-query = "What did the president say about Justice Breyer"
-rag_chain.invoke(query)
+query = "What did the president say about Justice Breyer?"
+print(rag_chain.invoke(query))
